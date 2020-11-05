3987d723a64038eb2fafe8a88eb65fbae3fcdd06
zxx <yahone.chow@gmail.com>
2020-11-05 18:09:22 +0800

    backup

--- content_scripts/index.js
+++ content_scripts/index.js
@@ -4,23 +4,26 @@ let reg = /baidu.com|google.com/g;
 let match = location.host.match(reg);
 if(match){
 	rule = filterRules[match[0].replace('.com', '')];
+
+	rule.removeAds();
+
+	// console.log(document.body);
+
+	const callback = function(mutationsList, observer) {
+		rule && rule.removeAds();
+		// console.log(mutationsList);
+		/*for(let mutation of mutationsList) {
+			if (mutation.type === 'childList') {
+				console.log('A child node has been added or removed.');
+			}
+		}*/
+	};
+
+	const observer = new MutationObserver(callback);
+
+	observer.observe(document.body, {attributes: false, childList: true, subtree: true});
+
 } else{
-	throw new Error('No rule here');
+	// console.warning('No rule here');
+	// throw new Error('No rule here');
 }
-rule.removeAds();
-
-// console.log(document.body);
-
-const callback = function(mutationsList, observer) {
-	rule && rule.removeAds();
-	// console.log(mutationsList);
-	/*for(let mutation of mutationsList) {
-		if (mutation.type === 'childList') {
-			console.log('A child node has been added or removed.');
-		}
-	}*/
-};
-
-const observer = new MutationObserver(callback);
-
-observer.observe(document.body, {attributes: false, childList: true, subtree: true});
